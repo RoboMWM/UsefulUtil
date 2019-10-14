058d640637b44d17b842584425e18b0f44c822ee
RoboMWM <robomwm@users.noreply.github.com>
2019-10-14 09:00:14 -0700

    Queue saving, instead of using a pool

--- src/main/java/com/robomwm/usefulutils/FileUtils.java
+++ src/main/java/com/robomwm/usefulutils/FileUtils.java
@@ -1,14 +1,15 @@
 package com.robomwm.usefulutils;
 
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.Collections;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 /**
  * Created on 8/3/2019.
@@ -17,7 +18,7 @@ import java.util.concurrent.Executors;
  */
 public final class FileUtils
 {
-    private static ExecutorService savePool = Executors.newCachedThreadPool();
+    private static BlockingQueue<Integer> saveQueue = new SynchronousQueue<>();
 
     /**
      * Asynchronsly save the specified string into a file within the plugin's data folder.
@@ -45,25 +46,39 @@ public final class FileUtils
      */
     public static void saveStringToFile(Plugin plugin, File storageFile, String contents)
     {
-        savePool.execute(() ->
+        new BukkitRunnable()
+        {
+            @Override
+            public void run()
+            {
+                try
                 {
-                    if (contents == null || contents.isEmpty())
-                    {
-                        storageFile.delete();
-                    }
-
-                    try
-                    {
-                        storageFile.getParentFile().mkdirs();
-                        storageFile.createNewFile();
-                        Files.write(storageFile.toPath(), Collections.singletonList(contents), StandardCharsets.UTF_8, StandardOpenOption.CREATE);
-                    }
-                    catch (Exception e)
-                    {
-                        plugin.getLogger().severe("Could not save " + storageFile.toString());
-                        e.printStackTrace();
-                    }
+                    saveQueue.put(this.hashCode());
                 }
-        );
+                catch (InterruptedException e)
+                {
+                    plugin.getLogger().severe("Could not save " + storageFile.toString());
+                    e.printStackTrace();
+                }
+
+
+                if (contents == null || contents.isEmpty())
+                {
+                    storageFile.delete();
+                }
+
+                try
+                {
+                    storageFile.getParentFile().mkdirs();
+                    storageFile.createNewFile();
+                    Files.write(storageFile.toPath(), Collections.singletonList(contents), StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+                }
+                catch (Exception e)
+                {
+                    plugin.getLogger().severe("Could not save " + storageFile.toString());
+                    e.printStackTrace();
+                }
+            }
+        }.runTaskAsynchronously(plugin);
     }
 }
